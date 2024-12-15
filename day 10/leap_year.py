da297126fd2463234413ed531802d049b2483c3e
Clinton Odia <odiamehiclinton@gmail.com>
2024-12-15 04:17:54 +0100

    Leap year challenge

---
+++ day 10/leap_year.py
@@ -0,0 +1,16 @@
+def is_leap_year(year):
+    # Write your code here. 
+    # Don't change the function name.
+    if year % 4 == 0:
+        if year % 100 != 0 or year % 400 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+    
+
+answer = (is_leap_year(2001))
+print(answer)
+            
