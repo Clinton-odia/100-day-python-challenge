11e6fb0af812bf7120b36fd0d45bf8adb95e618c
Clinton Odia <odiamehiclinton@gmail.com>
2024-12-15 20:26:57 +0100

    calculator project completed

--- day 10/calculator_project.py
+++ day 10/calculator_project.py
@@ -1,3 +1,4 @@
+from art import logo
 def add(n1, n2):
   return n1 + n2
 
@@ -10,11 +11,34 @@ def divide(n1,n2):
 def multiply(n1,n2):
   return n1 * n2
 
-calc = {
+operation = {
   "+": add,
   "-": substract,
   "*": multiply,
   "/": divide,
 }
 
-print(calc['*'](4,8))
+def calculations(first_num,calc,second_num):
+  for sign in operation:
+    if sign == calc:
+      return operation[sign](first_num,second_num)
+def calculator():
+  print(logo)
+  first_number = int(input("What's your first number? "))
+  should_accumulate = True
+  while should_accumulate:
+
+    calculation = input("+\n-\n*\n/\nPick an operation: ")
+    second_number = int(input("What's your next number? "))
+    answer = calculations(first_num=first_number, calc=calculation, second_num=second_number)
+    print(f"{first_number}{calculation}{second_number} = {answer}")
+
+    choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation: ").lower()
+    if choice == 'y':
+      first_number = answer
+    else:
+      should_accumulate = False
+      print("\n" * 20)
+      calculator()
+
+calculator()
