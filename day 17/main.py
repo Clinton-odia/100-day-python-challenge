5fb04daf962a501a7966892037325b1ddf8ffd38
Clinton Odia <odiamehiclinton@gmail.com>
2024-12-22 20:17:29 +0100

    third challenge completed

--- day 17/main.py
+++ day 17/main.py
@@ -1,9 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 question_bank = []
 for item in question_data:
   question_text = item["text"]
   question_answer = item["answer"]
   new_question = Question(question_text, question_answer)
   question_bank.append(new_question)
-print(question_bank[0].text)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+  quiz.next_question()
