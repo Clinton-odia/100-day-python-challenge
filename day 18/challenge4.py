a9f841038584b13c84e2a1e1ecf4784d1d779e23
Clinton Odia <odiamehiclinton@gmail.com>
2024-12-24 19:44:37 +0100

    challenge 5 completed

--- day 18/challenge4.py
+++ day 18/challenge4.py
@@ -1,15 +1,22 @@
 import random
-from turtle import Turtle, Screen
+import turtle
+from turtle import Turtle, Screen, colormode
 
 timmy_the_turtle = Turtle()
 
-colours = ["red", "blue", "brown", "SeaGreen", "SlateGray"]
+turtle.colormode(255)
+def random_color():
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  return (r,g,b)
+
 direction = [0, 90, 180, 270]
 timmy_the_turtle.pensize(12)
 timmy_the_turtle.speed("fastest")
 
 for _ in range(200):
-  timmy_the_turtle.color(random.choice(colours))
+  timmy_the_turtle.color(random_color())
   timmy_the_turtle.forward(30)
   timmy_the_turtle.setheading(random.choice(direction))
 
