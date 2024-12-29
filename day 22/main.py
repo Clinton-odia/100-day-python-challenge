085750d5db5ae8a0f14b3eca3759f3f544fba12d
Clinton Odia <odiamehiclinton@gmail.com>
2024-12-29 21:23:20 +0100

    scoreboard file added

--- day 22/main.py
+++ day 22/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+game_ball = Ball()
 
 screen.listen()
 screen.onkey(fun=r_paddle.UP, key= "Up")
@@ -18,7 +21,16 @@ screen.onkey(fun=l_paddle.UP, key= "w")
 screen.onkey(fun=l_paddle.DOWN, key= "s")
 game_is_on = True
 while game_is_on:
+  time.sleep(0.1)
   screen.update()
+  game_ball.move()
+  #detect collision with wall
+  if game_ball.ycor() > 280 or game_ball.ycor() < -280:
+    game_ball.bounce_y()
 
+  #detect collision with paddle
+
+  if  game_ball.distance(r_paddle) < 50 and game_ball.xcor() > 320 or game_ball.distance(l_paddle) and game_ball.xcor() < -320:
+    game_ball.bounce_x()
 
 screen.exitonclick()
