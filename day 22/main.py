7b7e06860c41bceb6423060ef3c7616dfc2bd0c9
Clinton Odia <odiamehiclinton@gmail.com>
2024-12-29 20:27:08 +0100

    paddle class created and the ball class

--- day 22/main.py
+++ day 22/main.py
@@ -1,4 +1,24 @@
 from turtle import Turtle, Screen
-
+from paddle import Paddle
 screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(fun=r_paddle.UP, key= "Up")
+screen.onkey(fun=r_paddle.DOWN, key= "Down")
+
+screen.onkey(fun=l_paddle.UP, key= "w")
+screen.onkey(fun=l_paddle.DOWN, key= "s")
+game_is_on = True
+while game_is_on:
+  screen.update()
+
+
 screen.exitonclick()
