bc7fc0ada18d49619ba38efb6e83f3ecdcf81cb7
Clinton Odia <odiamehiclinton@gmail.com>
2025-01-04 21:14:58 +0100

    day 26 and 25 starter files

--- day 24/Mail Merge Project Start/main.py
+++ day 24/Mail Merge Project Start/main.py
@@ -1,8 +1,8 @@
-#TODO: Create a letter using starting_letter.txt 
+
 with open(file=r".\Input\Letters\starting_letter.txt") as start_letter:
     letter = start_letter.read()
 print(letter)
-#for each name in invited_names.txt
+
 invited_name = open(file=r"Input\Names\invited_names.txt" )
 names = invited_name.readlines()
 
@@ -13,10 +13,3 @@ for name in names:
     with open(file=fr"Output\ReadyToSend\invitation_for_{new_name}", mode="w") as invitation:
         invitation.write(invite)
 
-    print(invite)
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
---
+++ day 25/main.py
@@ -0,0 +1,18 @@
+# with open(file="day 25\weather_data.csv") as weather_data:
+#   data = weather_data.readlines()
+#   print(data)
+
+# import csv
+
+# with open(file="day 25\weather_data.csv") as data_file:
+#   data = csv.reader(data_file)
+#   temperatures = []
+#   for row in data:
+#     if row[1] != "temp":
+#       row = int(row[1])
+#       temperatures.append(row)
+    
+
+#   print(temperatures)
+
+import pandas
