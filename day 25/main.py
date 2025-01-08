8362441bc942eb4f3effe9387accc58e883b50d5
Clinton Odia <odiamehiclinton@gmail.com>
2025-01-08 18:52:41 +0100

    US STATE GAME STARTER FILE

--- day 25/main.py
+++ day 25/main.py
@@ -20,3 +20,19 @@ import pandas
 # data = pandas.read_csv("day 25\weather_data.csv")
 # temp_list =(data[data.temp == data["temp"].max()])
 # print(temp_list)
+
+data = pandas.read_csv("day 25\\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrel_count)
+print(red_squirrel_count)
+print(black_squirrel_count)
+
+my_dict = {
+  "fur color": ["Gray", "Cinnamon", "Black"],
+  "Count" : [grey_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+print(my_dict)
+pf = pandas.DataFrame(my_dict)
+pf.to_csv("squirrel_count.csv")
